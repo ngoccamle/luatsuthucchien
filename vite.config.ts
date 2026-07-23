d81abb538d001543c2832ea37497aef1829f7b74
ngoccamle <ngoccamletx@gmail.com>
2026-07-23 21:26:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/luatsuthucchien/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
